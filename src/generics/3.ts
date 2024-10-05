3cc5cfafd249403475813d48b922365001e17e8f
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-10-05 20:30:58 +0200

    upd

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,6 +1,6 @@
-function merge<T extends object, U extends object> (objA: T, objB: U): T&U {
-      return Object.assign(objA, objB);
-    }
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+    return Object.assign({},objA, objB);
+}
     const object1 = { name: 'Max', age: 30 };
 const object2 = { city: 'New York', country: 'USA' };
 
